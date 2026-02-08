23ea64cbd623ca71ab1960d4e75f0aca1a615563
dingemoe <108612432+dingemoe@users.noreply.github.com>
2026-02-08 00:02:54 +0000

    safa

---
+++ index.js
@@ -0,0 +1,11 @@
+// Komponent for bruk i eksisterende React userscript/Tampermonkey prosjekt
+const App = () => {
+  return (
+    <div className="userscript-app">
+      <h1>React Userscript App</h1>
+    </div>
+  );
+};
+
+export default App;
+export { App as UserscriptApp };
